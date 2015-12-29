56678c931727a412e173e36e5f0827161ad0862e
Jonathan Litfin <jonathan.litfin@payspan.com>
2015-12-29 14:29:31 -0500

    Initial commit db extract

---
+++ DbExtractTest/DbExtractTest/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbExtractTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //using (var sr = new StreamReader("actors.list.gz"))
+            //using (var sw = new StreamWriter("actors.dat"))
+            using (var sr = new StreamReader("movies.list.gz"))
+            using (var sw = new StreamWriter("movies.dat"))
+            {
+                
+                for (int i = 0; i < 564046; ++i)
+                {
+                    if (i >= 564042)
+                    {
+                        sw.WriteLine(sr.ReadLine());
+                    }
+                    else
+                    {
+                        sr.ReadLine();
+                    }
+                }
+                
+            }
+
+
+            Console.WriteLine("Done.");
+            Console.Read();
+        }
+    }
+}
