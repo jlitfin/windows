066a030f310a3d03e569d71e0d0488cc6a0e2758
Jonathan Litfin <Jonathan Litfin>
2017-04-12 21:56:26 -0400

    deleting rubbish

--- msbuild/scripts/cleaniis.bat
+++
@@ -1,8 +0,0 @@
-IISRESET /STOP
-FOR /f "delims=" %%i in ('dir /AD /D /B /Q "C:\Windows\Microsoft.NET\Framework\v2.0.50727\Temporary ASP.NET Files"') DO RD /s /q "C:\Windows\Microsoft.NET\Framework\v2.0.50727\Temporary ASP.NET Files\%%i" 
-FOR /f "delims=" %%i in ('dir /AD /D /B /Q "C:\Windows\Microsoft.NET\Framework64\v2.0.50727\Temporary ASP.NET Files"') DO RD /s /q "C:\Windows\Microsoft.NET\Framework64\v2.0.50727\Temporary ASP.NET Files\%%i" 
-FOR /f "delims=" %%i in ('dir /AD /D /B /Q "C:\Windows\Microsoft.NET\Framework\v4.0.30319\Temporary ASP.NET Files"') DO RD /s /q "C:\Windows\Microsoft.NET\Framework\v4.0.30319\Temporary ASP.NET Files\%%i" 
-FOR /f "delims=" %%i in ('dir /AD /D /B /Q "C:\Windows\Microsoft.NET\Framework64\v4.0.30319\Temporary ASP.NET Files"') DO RD /s /q "C:\Windows\Microsoft.NET\Framework64\v4.0.30319\Temporary ASP.NET Files\%%i" 
-IISRESET /START
-
-ECHO Work Complete.
